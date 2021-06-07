nickodell/rmyloginpath

commit 52f31e2eb6636e533224f5f985e5c0680ef472ed

Fix lint

R/rmyloginpath.R
@@ -52,7 +52,7 @@ login_parse <- function(login_path, path = NULL) {
 get_login_path_file <- function() {
   default <- file.path("~", ".mylogin.cnf")
   # TODO: Windows stores its config file in APPDATA
-  Sys.getenv("MYSQL_TEST_LOGIN_FILE", unset=default)
+  Sys.getenv("MYSQL_TEST_LOGIN_FILE", unset = default)
 }
 
 read_bytes <- function(fp, n) {
@@ -61,7 +61,9 @@ read_bytes <- function(fp, n) {
 }
 
 read_length <- function(fp) {
-  len <- readBin(fp, "integer", n = 1, size = cipher_store_length, endian = "little")
+  len <- readBin(fp, "integer", n = 1
+                 size = cipher_store_length,
+                 endian = "little")
   if (length(len) == 0) {
     # Failed to read length. End of file
     -1

R/parse_config.R
@@ -40,7 +40,7 @@ read_mysql_ini <- function(fileText) {
 
   ini <- list()
 
-  lines = strsplit(fileText, "\n", fixed = TRUE)[[1]]
+  lines <- strsplit(fileText, "\n", fixed = TRUE)[[1]]
 
   for (line in lines) {
 

commit b162f9edd6499480c848799c5c8631f97fe6601d

Fix parse error introduced in 52f31e2

R/rmyloginpath.R
@@ -61,7 +61,7 @@ read_bytes <- function(fp, n) {
 }
 
 read_length <- function(fp) {
-  len <- readBin(fp, "integer", n = 1
+  len <- readBin(fp, "integer", n = 1,
                  size = cipher_store_length,
                  endian = "little")
   if (length(len) == 0) {